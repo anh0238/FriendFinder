8392d863cd3a33939724b12367167a0b23df10db
ahend <anh0238@gmail.com>
2018-12-17 22:35:27 -0600

    dirname error, fix attempt

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,14 +4,15 @@ module.exports = function(app) {
 
     // A GET Route to `/survey` which should display the survey page.
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(_dirname, "/../public/survey.html"));
+        res.sendFile(path.join(_dirname, "/./public/survey.html"));
     });
 
     // A default, catch-all route that leads to `home.html` which displays the home page. 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(_dirname, "/../public/home.html"));
+        res.sendFile(path.join(_dirname, "/./public/home.html"));
     });
 
 };
+
 
 

afd834a23b30ff5d745113dc0020ec5e43de9c73
ahend <anh0238@gmail.com>
2018-12-17 22:33:12 -0600

    _dirname edited, htmlRoutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 
     // A default, catch-all route that leads to `home.html` which displays the home page. 
     app.get("*", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/home.html"));
+        res.sendFile(path.join(_dirname, "/../public/home.html"));
     });
 
 };

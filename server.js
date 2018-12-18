b4498f8c1fe12508fb5f0e54c227760a76870429
ahend <anh0238@gmail.com>
2018-12-17 22:17:16 -0600

    server.js routes and listener added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require('path');
+var path = require('path'); //look into whether or not this needs to be called on server.js file. per instructions, needed. 
 
 var app = express();
 
@@ -7,3 +7,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
